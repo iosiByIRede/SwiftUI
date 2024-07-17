570a30409a5db3926fe86528fbe35bc4e11677df
Ian Pacini <124642552+IanPacini123@users.noreply.github.com>
2024-07-17 14:28:21 -0300

    created the view to be worked on

---
+++ RPGCharacter/RPGCharacter/NewCharacterView.swift
@@ -0,0 +1,18 @@
+//
+//  NewCharacterView.swift
+//  RPGCharacter
+//
+//  Created by Ian Pacini on 17/07/24.
+//
+
+import SwiftUI
+
+struct NewCharacterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewCharacterView()
+}
