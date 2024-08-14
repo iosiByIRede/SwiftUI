e2370f53206abe114bcaec61a608ddf54a17135c
Ian Pacini <124642552+IanPacini123@users.noreply.github.com>
2024-08-14 11:37:47 -0300

    end of 4th class

--- RPGCharacter/RPGCharacter/OnboardingView.swift
+++ RPGCharacter/RPGCharacter/OnboardingView.swift
@@ -9,24 +9,8 @@ import SwiftUI
 
 struct OnboardingView: View {
     var body: some View {
-        CustomShape()
-            .frame(width: 200, height: 120)
-            .foregroundStyle(.orange)
-    }
-}
-
-struct CustomShape: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        let size = max(rect.width, rect.height)
-        let rect = CGRect(origin: rect.origin, size: CGSize(width: size, height: size))
-        // Desenhar uma forma personalizada
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.closeSubpath()
-        
-        return path
+        LinearGradient(colors: [.rpgDarkBlue, .rpgGrayedBlue, .rpgBlack, .red], startPoint: .top, endPoint: .bottom)
+            .ignoresSafeArea()
     }
 }
 
