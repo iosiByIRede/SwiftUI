a677383df7324031104545f39b87740012e7c909
Ian Pacini <124642552+IanPacini123@users.noreply.github.com>
2024-08-14 21:28:01 -0300

    ent of 6th class

--- RPGCharacter/RPGCharacter/OnboardingView.swift
+++ RPGCharacter/RPGCharacter/OnboardingView.swift
@@ -9,8 +9,46 @@ import SwiftUI
 
 struct OnboardingView: View {
     var body: some View {
-        LinearGradient(colors: [.rpgDarkBlue, .rpgGrayedBlue, .rpgBlack, .red], startPoint: .top, endPoint: .bottom)
+        ZStack {
+            ZStack {
+                Image("bricksBG")
+                    .resizable()
+                
+                LinearGradient(colors: [.rpgGrayedBlue, .rpgLightBrown], startPoint: .top, endPoint: .bottom)
+                    .opacity(0.7)
+            }
             .ignoresSafeArea()
+            
+            VStack {
+                Image("OnboardingRPG")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 260)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+                Text("Seja bem vindo!")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .bold()
+                
+                Button(action: {}, label: {
+                    HStack {
+                        Text("Vamos lá")
+                        
+                        Image(systemName: "figure.archery")
+                    }
+                    .foregroundStyle(.black)
+                    .bold()
+                    .font(.title)
+                })
+                .padding(20)
+                .padding(.horizontal, 40)
+                .background {
+                    RoundedRectangle(cornerRadius: 20)
+                        .foregroundStyle(.white)
+                }
+            }
+        }
     }
 }
 
